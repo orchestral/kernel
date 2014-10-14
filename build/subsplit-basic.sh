c9000c9ecc6f7ec42b964da13e854f9c28c2eaa7
crynobone <crynobone@gmail.com>
2014-10-14 11:22:29 +0800

    Update subsplit build.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- build/subsplit-basic.sh
+++ build/subsplit-basic.sh
@@ -1,3 +1,4 @@
 git subsplit init git@github.com:orchestral/kernel.git
+git subsplit publish --no-tags src/Contracts:git@github.com:orchestral/contracts.git
 git subsplit publish --no-tags src/Http:git@github.com:orchestral/http.git
 rm -rf .subsplit/
