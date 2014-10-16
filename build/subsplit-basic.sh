33bd54a23bf4411ddf90e7a35c10b1a5dd477aa2
crynobone <crynobone@gmail.com>
2014-10-16 08:00:14 +0800

    Add routing subsplit.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- build/subsplit-basic.sh
+++ build/subsplit-basic.sh
@@ -1,4 +1,5 @@
 git subsplit init git@github.com:orchestral/kernel.git
 git subsplit publish --no-tags src/Contracts:git@github.com:orchestral/contracts.git
 git subsplit publish --no-tags src/Http:git@github.com:orchestral/http.git
+git subsplit publish --no-tags src/Routing:git@github.com:orchestral/routing.git
 rm -rf .subsplit/
--- build/subsplit-full.sh
+++ build/subsplit-full.sh
@@ -1,4 +1,5 @@
 git subsplit init git@github.com:orchestral/kernel.git
 git subsplit publish src/Contracts:git@github.com:orchestral/contracts.git
 git subsplit publish src/Http:git@github.com:orchestral/http.git
+git subsplit publish src/Routing:git@github.com:orchestral/routing.git
 rm -rf .subsplit/
