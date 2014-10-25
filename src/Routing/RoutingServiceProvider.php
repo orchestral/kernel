71def4466557fad7d22a971f88871635a64df1b8
crynobone <crynobone@gmail.com>
2014-10-25 09:13:49 +0800

    Revert registering request for console.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Routing/RoutingServiceProvider.php
+++ src/Routing/RoutingServiceProvider.php
@@ -5,34 +5,6 @@ use Illuminate\Http\Request;
 class RoutingServiceProvider extends \Illuminate\Routing\RoutingServiceProvider
 {
     /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->registerRequestOnConsole();
-
-        parent::register();
-    }
-
-    /**
-     * Register the requst on console interface.
-     *
-     * @return void
-     */
-    protected function registerRequestOnConsole()
-    {
-        $app = $this->app;
-
-        if ($app->runningInConsole()) {
-            $url = $app['config']->get('app.url', 'http://localhost');
-
-            $app->instance('request', Request::create($url, 'GET', [], [], [], $_SERVER));
-        }
-    }
-
-    /**
      * Register the router instance.
      *
      * @return void
