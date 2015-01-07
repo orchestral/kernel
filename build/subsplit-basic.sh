5bcae476d2528880e05451d5b664187029ca2bef
crynobone <crynobone@gmail.com>
2015-01-07 10:41:47 +0800

    Add orchestra/database composer.json
    Signed-off-by: crynobone <crynobone@gmail.com>

--- build/subsplit-basic.sh
+++ build/subsplit-basic.sh
@@ -1,6 +1,8 @@
+#!/bin/sh
 git subsplit init git@github.com:orchestral/kernel.git
 git subsplit publish --no-tags src/Config:git@github.com:orchestral/config.git
 git subsplit publish --no-tags src/Contracts:git@github.com:orchestral/contracts.git
+git subsplit publish --no-tags src/Database:git@github.com:orchestral/database.git
 git subsplit publish --no-tags src/Http:git@github.com:orchestral/http.git
 git subsplit publish --no-tags src/Routing:git@github.com:orchestral/routing.git
 rm -rf .subsplit/
--- build/subsplit-full.sh
+++ build/subsplit-full.sh
@@ -1,6 +1,8 @@
+#!/bin/sh
 git subsplit init git@github.com:orchestral/kernel.git
 git subsplit publish src/Config:git@github.com:orchestral/config.git
 git subsplit publish src/Contracts:git@github.com:orchestral/contracts.git
+git subsplit publish src/Database:git@github.com:orchestral/database.git
 git subsplit publish src/Http:git@github.com:orchestral/http.git
 git subsplit publish src/Routing:git@github.com:orchestral/routing.git
 rm -rf .subsplit/
