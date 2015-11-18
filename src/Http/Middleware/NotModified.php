4a1587026c496847a986c51965e23d645de3669a
crynobone <crynobone@gmail.com>
2015-11-18 17:48:42 +0800

    Add NotModified middleware.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Http/Middleware/NotModified.php
@@ -0,0 +1,26 @@
+<?php namespace Orchestra\Http\Middleware;
+
+use Closure;
+
+class NotModified
+{
+    /**
+     * Handle the given request and get the response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return \Illuminate\Http\Response
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        if ( ! $response->headers->has('Etag')) {
+            $response->setEtag(md5($response->getContent()));
+        }
+
+        $response->isNotModified($request);
+
+        return $response;
+    }
+}
