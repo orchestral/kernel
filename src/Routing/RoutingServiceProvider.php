747b9daf1ff70239ad19de80693db6a73c5d5d90
crynobone <crynobone@gmail.com>
2016-12-23 07:50:53 +0800

    Update to use singleton.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Routing/RoutingServiceProvider.php
+++ src/Routing/RoutingServiceProvider.php
@@ -14,7 +14,7 @@ class RoutingServiceProvider extends ServiceProvider
      */
     protected function registerRouter()
     {
-        $this->app['router'] = $this->app->share(function (Application $app) {
+        $this->app->singleton('router', function (Application $app) {
             return new Router($app->make('events'), $app);
         });
     }
