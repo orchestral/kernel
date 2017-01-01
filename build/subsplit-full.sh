692f89da42de91c8788bc7c2248e70b024eafebc
crynobone <crynobone@gmail.com>
2017-01-01 11:21:56 +0800

    Update subsplit command.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- build/subsplit-full.sh
+++ build/subsplit-full.sh
@@ -6,8 +6,8 @@ else
     git subsplit init git@github.com:orchestral/kernel.git
 fi
 
-git subsplit publish --heads="master 3.3 3.2 3.1" src/Config:git@github.com:orchestral/config.git
-git subsplit publish --heads="master 3.3 3.2 3.1" src/Database:git@github.com:orchestral/database.git
-git subsplit publish --heads="master 3.3 3.2 3.1" src/Http:git@github.com:orchestral/http.git
-git subsplit publish --heads="master 3.3" src/Notifications:git@github.com:orchestral/notifications.git
-git subsplit publish --heads="master 3.3 3.2 3.1" src/Routing:git@github.com:orchestral/routing.git
+git subsplit publish --heads="master 3.4 3.3 3.1" src/Config:git@github.com:orchestral/config.git
+git subsplit publish --heads="master 3.4 3.3 3.1" src/Database:git@github.com:orchestral/database.git
+git subsplit publish --heads="master 3.4 3.3 3.1" src/Http:git@github.com:orchestral/http.git
+git subsplit publish --heads="master 3.4 3.3" src/Notifications:git@github.com:orchestral/notifications.git
+git subsplit publish --heads="master 3.4 3.3 3.1" src/Routing:git@github.com:orchestral/routing.git
