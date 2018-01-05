fb4283266802616385849fba4fe9306b80b89cd8
Mior Muhammad Zaki <crynobone@gmail.com>
2018-01-05 11:30:33 +0800

    Prepare 3.6 branch.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- build/subsplit-basic.sh
+++ build/subsplit-basic.sh
@@ -6,8 +6,8 @@ else
     git subsplit init git@github.com:orchestral/kernel.git
 fi
 
-git subsplit publish --heads="master 3.5 3.4 3.1" --no-tags src/Config:git@github.com:orchestral/config.git
-git subsplit publish --heads="master 3.5 3.4 3.1" --no-tags src/Database:git@github.com:orchestral/database.git
-git subsplit publish --heads="master 3.5 3.4 3.1" --no-tags src/Http:git@github.com:orchestral/http.git
-git subsplit publish --heads="master 3.5 3.4" --no-tags src/Notifications:git@github.com:orchestral/notifications.git
-git subsplit publish --heads="master 3.5 3.4 3.1" --no-tags src/Routing:git@github.com:orchestral/routing.git
+git subsplit publish --heads="master 3.6 3.5 3.1" --no-tags src/Config:git@github.com:orchestral/config.git
+git subsplit publish --heads="master 3.6 3.5 3.1" --no-tags src/Database:git@github.com:orchestral/database.git
+git subsplit publish --heads="master 3.6 3.5 3.1" --no-tags src/Http:git@github.com:orchestral/http.git
+git subsplit publish --heads="master 3.6 3.5" --no-tags src/Notifications:git@github.com:orchestral/notifications.git
+git subsplit publish --heads="master 3.6 3.5 3.1" --no-tags src/Routing:git@github.com:orchestral/routing.git
