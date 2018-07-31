1e1879594bd349d7a89243a3538b45dba8e41aad
Mior Muhammad Zaki <crynobone@gmail.com>
2018-07-31 22:50:06 +0800

    Prepare 3.7 branch.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- build/subsplit-full.sh
+++ build/subsplit-full.sh
@@ -6,8 +6,8 @@ else
     git subsplit init git@github.com:orchestral/kernel.git
 fi
 
-git subsplit publish --heads="3.6" src/Config:git@github.com:orchestral/config.git
-git subsplit publish --heads="3.6" src/Database:git@github.com:orchestral/database.git
-git subsplit publish --heads="3.6" src/Http:git@github.com:orchestral/http.git
-git subsplit publish --heads="3.6" src/Notifications:git@github.com:orchestral/notifications.git
-git subsplit publish --heads="3.6" src/Routing:git@github.com:orchestral/routing.git
+git subsplit publish --heads="3.7" src/Config:git@github.com:orchestral/config.git
+git subsplit publish --heads="3.7" src/Database:git@github.com:orchestral/database.git
+git subsplit publish --heads="3.7" src/Http:git@github.com:orchestral/http.git
+git subsplit publish --heads="3.7" src/Notifications:git@github.com:orchestral/notifications.git
+git subsplit publish --heads="3.7" src/Routing:git@github.com:orchestral/routing.git
--- build/subsplit-tag.sh
+++ build/subsplit-tag.sh
@@ -11,8 +11,8 @@ else
     git subsplit init git@github.com:orchestral/kernel.git
 fi
 
-git subsplit publish --heads="3.6" --tags=$1 src/Config:git@github.com:orchestral/config.git
-git subsplit publish --heads="3.6" --tags=$1 src/Database:git@github.com:orchestral/database.git
-git subsplit publish --heads="3.6" --tags=$1 src/Http:git@github.com:orchestral/http.git
-git subsplit publish --heads="3.6" --tags=$1 src/Notifications:git@github.com:orchestral/notifications.git
-git subsplit publish --heads="3.6" --tags=$1 src/Routing:git@github.com:orchestral/routing.git
+git subsplit publish --heads="3.7" --tags=$1 src/Config:git@github.com:orchestral/config.git
+git subsplit publish --heads="3.7" --tags=$1 src/Database:git@github.com:orchestral/database.git
+git subsplit publish --heads="3.7" --tags=$1 src/Http:git@github.com:orchestral/http.git
+git subsplit publish --heads="3.7" --tags=$1 src/Notifications:git@github.com:orchestral/notifications.git
+git subsplit publish --heads="3.7" --tags=$1 src/Routing:git@github.com:orchestral/routing.git
