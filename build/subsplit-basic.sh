aec17b750f6c40d71ebe71d9e1870690038b6038
Mior Muhammad Zaki <crynobone@gmail.com>
2019-07-11 06:54:52 +0800

    Prepare 3.9 branch
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- build/subsplit-basic.sh
+++ build/subsplit-basic.sh
@@ -6,9 +6,9 @@ else
     git subsplit init git@github.com:orchestral/kernel.git
 fi
 
-git subsplit publish --heads="master" --no-tags src/Config:git@github.com:orchestral/config.git
-git subsplit publish --heads="master" --no-tags src/Database:git@github.com:orchestral/database.git
-git subsplit publish --heads="master" --no-tags src/Hashing:git@github.com:orchestral/hashing.git
-git subsplit publish --heads="master" --no-tags src/Http:git@github.com:orchestral/http.git
-git subsplit publish --heads="master" --no-tags src/Notifications:git@github.com:orchestral/notifications.git
-git subsplit publish --heads="master" --no-tags src/Routing:git@github.com:orchestral/routing.git
+git subsplit publish --heads="3.9" --no-tags src/Config:git@github.com:orchestral/config.git
+git subsplit publish --heads="3.9" --no-tags src/Database:git@github.com:orchestral/database.git
+git subsplit publish --heads="3.9" --no-tags src/Hashing:git@github.com:orchestral/hashing.git
+git subsplit publish --heads="3.9" --no-tags src/Http:git@github.com:orchestral/http.git
+git subsplit publish --heads="3.9" --no-tags src/Notifications:git@github.com:orchestral/notifications.git
+git subsplit publish --heads="3.9" --no-tags src/Routing:git@github.com:orchestral/routing.git
