cbf334ce08c49600b75b7f4259690da5f9787b3f
Mior Muhammad Zaki <crynobone@gmail.com>
2019-07-23 15:04:17 +0800

    Add InteractsWithDatetime helper.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ src/Http/Transformer/InteractsWithDateTime.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Orchestra\Http\Transformer;
+
+use Carbon\Carbon;
+
+trait InteractsWithDateTime
+{
+    /**
+     * The request timezone code cache.
+     *
+     * @var null
+     */
+    protected static $requestHeaderTimezoneCode = null;
+
+    /**
+     * Get timezone from request header.
+     *
+     * @return string
+     */
+    protected function getTimezoneFromRequestHeader(): string
+    {
+        if (\is_null(static::$requestHeaderTimezoneCode)) {
+            static::$requestHeaderTimezoneCode = \optional($this->getRequest())->header('time-zone') ?? 'UTC';
+        }
+
+        return static::$requestHeaderTimezoneCode;
+    }
+
+    /**
+     * Convert Carbon to datetime string or return null.
+     *
+     * @param \Carbon\Carbon|null $datetime
+     *
+     * @return string|null
+     */
+    protected function toDateString(Carbon $datetime = null)
+    {
+        return $datetime instanceof Carbon
+                    ? $datetime->timezone($this->getTimezoneFromRequestHeader())->toDateString()
+                    : null;
+    }
+
+    /**
+     * Convert Carbon to datetime string or return null.
+     *
+     * @param \Carbon\Carbon|null $datetime
+     *
+     * @return string|null
+     */
+    protected function toDatetimeString(Carbon $datetime = null)
+    {
+        return $datetime instanceof Carbon
+                    ? $datetime->timezone($this->getTimezoneFromRequestHeader())->toDatetimeString()
+                    : null;
+    }
+}
