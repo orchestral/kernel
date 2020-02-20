74394ab57ffc66145eeb20fa0e8f4ec8fea8075d
Mior Muhammad Zaki <crynobone@gmail.com>
2020-02-20 22:07:23 +0800

    Prepare 5.x branch
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- build/subsplit-full.sh
+++ build/subsplit-full.sh
@@ -6,9 +6,9 @@ else
     git subsplit init git@github.com:orchestral/kernel.git
 fi
 
-git subsplit publish --heads="master" src/Config:git@github.com:orchestral/config.git
-git subsplit publish --heads="master" src/Database:git@github.com:orchestral/database.git
-git subsplit publish --heads="master" src/Hashing:git@github.com:orchestral/hashing.git
-git subsplit publish --heads="master" src/Http:git@github.com:orchestral/http.git
-git subsplit publish --heads="master" src/Notifications:git@github.com:orchestral/notifications.git
-git subsplit publish --heads="master" src/Routing:git@github.com:orchestral/routing.git
+git subsplit publish --heads="5.x" src/Config:git@github.com:orchestral/config.git
+git subsplit publish --heads="5.x" src/Database:git@github.com:orchestral/database.git
+git subsplit publish --heads="5.x" src/Hashing:git@github.com:orchestral/hashing.git
+git subsplit publish --heads="5.x" src/Http:git@github.com:orchestral/http.git
+git subsplit publish --heads="5.x" src/Notifications:git@github.com:orchestral/notifications.git
+git subsplit publish --heads="5.x" src/Routing:git@github.com:orchestral/routing.git
