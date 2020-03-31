0466751db6055335585d00cf791884b0a503f03c
Mior Muhammad Zaki <crynobone@gmail.com>
2020-03-31 08:45:31 +0800

    Remove auth router.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Routing/Router.php
+++ src/Routing/Router.php
@@ -9,36 +9,6 @@ use Illuminate\Routing\Router as BaseRouter;
 class Router extends BaseRouter
 {
     /**
-     * Register the typical authentication routes for an application.
-     *
-     * @return void
-     */
-    public function auth(array $options = [])
-    {
-        // Authentication Routes...
-        $this->get('login', 'Auth\AuthenticateController@show');
-        $this->post('login', 'Auth\AuthenticateController@attempt');
-        $this->get('logout', 'Auth\DeauthenticateController@logout');
-
-        $this->get('register', 'Auth\RegisterController@show');
-        $this->post('register', 'Auth\RegisterController@store');
-    }
-
-    /**
-     * Register the typical password reset routes for an application.
-     *
-     * @return void
-     */
-    public function password()
-    {
-        // Password Reset Routes...
-        $this->get('password/reset/{token?}', 'Auth\PasswordController@showResetForm');
-        $this->post('password/reset', 'Auth\PasswordController@reset');
-        $this->get('password/email', 'Auth\PasswordController@showLinkRequestForm');
-        $this->post('password/email', 'Auth\PasswordController@sendResetLinkEmail');
-    }
-
-    /**
      * Route a resource to a controller.
      *
      * @param  string  $name
